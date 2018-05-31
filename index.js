ec1e6bc854887ccd1f8958f7345691457fcc898b
Vicky Singh <vicky.victus@gmail.com>
2018-05-31 19:15:52 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var port = process.env.PORT || 8080;
 app.post('/',(req,res)=>{
   let response = "This is a sample response from your webhook!"; //Default response from the webhook to show it’s working
   let responseObj={ "fulfillmentText":response ,
-                   "fulfillmentMessages":[ { "text": { "text": [ "Hello I m Responding to intent" ] } } ] ,
+                   "fulfillmentMessages":[ { "text": { "text": [ "his number is 9537759063" ] } } ] ,
                    "source":"" } 
   return res.json(responseObj);});
 
