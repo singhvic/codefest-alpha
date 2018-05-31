4068fe2e527449ee3e65aaf060ae75868267a82b
Vicky Singh <vicky.victus@gmail.com>
2018-05-31 23:16:14 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const express = require('express')
-const app = express()
-app.use(express.bodyParser());
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 var port = process.env.PORT || 8080;
 
 var data = {
