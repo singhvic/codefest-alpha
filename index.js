aa911097714d251407340a5199e14538f8e6ae10
Vicky Singh <vicky.victus@gmail.com>
2018-05-31 20:55:49 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,20 @@ var port = process.env.PORT || 8080;
 
 app.post('/',(req,res)=>{
   //let response = "his number is 9537759063"; //Default response from the webhook to show it’s working
-  let responseObj={ "fulfillmentText":"his number is 9537759063" ,
+  /*let responseObj={ "fulfillmentText":"his number is 9537759063" ,
                    "fulfillmentMessages":[ { "text": { "text": [ "his number is 9537759063" ] } } ] ,
-                   "source":"" } 
+                   "source":"" } */
+  let responseObj={
+   "fulfillmentText":"his number is 9537759063",
+   "fulfillmentMessages":[
+      {
+         "text":[
+            "his number is 9537759063"
+         ]
+      }
+   ],
+   "source":""
+}
   return res.json(responseObj);});
 
 app.listen(port);
