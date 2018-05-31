f28048664485f0db30761726c52810ddf1959024
Vicky Singh <vicky.victus@gmail.com>
2018-05-31 19:10:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 var port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => res.send('Hello abc!'))
+app.post('/',(req,res)=>{
+  let response = "This is a sample response from your webhook!"; //Default response from the webhook to show it’s working
+  let responseObj={ "fulfillmentText":response ,
+                   "fulfillmentMessages":[ { "text": { "text": [ "Hello I m Responding to intent" ] } } ] ,
+                   "source":"" } 
+  return res.json(responseObj);});
 
 app.listen(port);
-
