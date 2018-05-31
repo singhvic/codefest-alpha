f39b6290b58bcb8c6052938717cee1c9b88ec1bd
Vicky Singh <vicky.victus@gmail.com>
2018-05-31 23:00:49 +0000

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.post('/',(req,res)=>{
   /*let responseObj={ "fulfillmentText":"his number is 9537759063" ,
                    "fulfillmentMessages":[ { "text": { "text": [ "his number is 9537759063" ] } } ] ,
                    "source":"" } */
-  console.log(req);
+  console.log(req.body);
   let responseObj={
    "fulfillmentText":"",
    "fulfillmentMessages":[
